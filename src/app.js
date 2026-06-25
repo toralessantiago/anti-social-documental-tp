14c7b013d135a485f907445b4bef27827cbc2cd4
sofiaagomez <sofiaxgomez97@gmail.com>
2026-06-25 17:50:55 -0300

    fix: extensión de swagger correcta

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -17,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const swaggerDocument = YAML.load(
-  fs.readFileSync(path.join(__dirname, "swagger.yml"), "utf8")
+  fs.readFileSync(path.join(__dirname, "swagger.yaml"), "utf8"),
 );
 
 app.use(express.json());

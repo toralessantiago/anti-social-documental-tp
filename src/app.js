d82dc4470fda138b43ac7f0ac251e6f0ac0d92e3
gonza <herleingonzaiao@gmail.com>
2026-06-25 11:25:10 -0300

    fix: ruta del swagger arreglada

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const swaggerDocument = YAML.load(
-  fs.readFileSync(path.join(__dirname, "../swagger.yml"), "utf8")
+  fs.readFileSync(path.join(__dirname, "swagger.yml"), "utf8")
 );
 
 app.use(express.json());

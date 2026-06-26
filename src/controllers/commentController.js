8420f61c93a77f74ff3514fdee80c756971d0303
estefaniaalmiron <estefaniaabigail.almiron@estudiantes.unahur.edu.ar>
2026-06-26 11:52:20 -0300

    fix: renombrar variable de entorno a LIMIT_MONTHS

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -2,7 +2,7 @@ const Comment = require("../models/Comment");
 
 const obtenerComentarios = async (req, res) => {
   try {
-    const meses = parseInt(process.env.MESES_VISIBLES) || 6;
+    const meses = parseInt(process.env.LIMIT_MONTHS) || 6;
     const fechaLimite = new Date();
     fechaLimite.setMonth(fechaLimite.getMonth() - meses);
 
@@ -19,7 +19,7 @@ const obtenerComentarios = async (req, res) => {
 
 const obtenerComentariosPorPost = async (req, res) => {
   try {
-    const meses = parseInt(process.env.MESES_VISIBLES) || 6;
+    const meses = parseInt(process.env.LIMIT_MONTHS) || 6;
     const fechaLimite = new Date();
     fechaLimite.setMonth(fechaLimite.getMonth() - meses);
 
